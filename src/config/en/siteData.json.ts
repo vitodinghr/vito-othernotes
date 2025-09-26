8c23617c29bd39e210db58d6032451eabcc7da18
Vito-Ding <haoran.ding@yahoo.com>
2025-09-27 01:42:19 +0800

    update

--- src/config/en/siteData.json.ts
+++ src/config/en/siteData.json.ts
@@ -2,15 +2,15 @@ import { type SiteDataProps } from "../types/configDataTypes";
 
 // Update this file with your site specific information
 const siteData: SiteDataProps = {
-	name: "VITODING Notes",
+	name: "VITODING.Notes",
 	// Your website's title and description (meta fields)
-	title: "Reading Notes Beyond Book Reviews",
+	title: "VITODING.Notes",
 	description:
 		"Reading Notes Beyond Book Reviews - Dive into a world of insights, reflections, and knowledge as I share my reading journey through detailed notes and thoughtful analyses. Explore a curated collection of book summaries, thematic explorations, and personal takeaways that go beyond traditional reviews. Join me in uncovering the essence of each book and discovering new perspectives along the way.",
 	// Your information!
 	author: {
-		name: "Cosmic Themes",
-		email: "creator@cosmicthemes.com",
+		name: "Vito",
+		email: "haoran.ding@yahoo.com",
 		twitter: "Cosmic_Themes",
 	},
 
