7c1f6b411e305048cddf8852e960bc1e87c3cf2a
Vito-Ding <haoran.ding@yahoo.com>
2025-09-26 11:08:42 +0800

    Update siteData.json.ts

--- src/config/en/siteData.json.ts
+++ src/config/en/siteData.json.ts
@@ -2,7 +2,7 @@ import { type SiteDataProps } from "../types/configDataTypes";
 
 // Update this file with your site specific information
 const siteData: SiteDataProps = {
-	name: "VitoDing Notes",
+	name: "VITODING Notes",
 	// Your website's title and description (meta fields)
 	title: "Reading Notes Beyond Book Reviews",
 	description:
